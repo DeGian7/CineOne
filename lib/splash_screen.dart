aad55b3cb66009892cca1fe1c08966ea029ea45a
DeGian <degiant7@gmail.com>
2022-06-22 22:53:58 -0500

    CineOne

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     Future.delayed(
-      Duration(milliseconds: 14000),
+      Duration(milliseconds: 12500),
       () => Navigator.pushReplacement(
         context,
         MaterialPageRoute(
